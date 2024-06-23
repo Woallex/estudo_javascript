c2a9ae57d08696fb5ad0883dba85cd4dd5ef0432
Woallex <alexpl.vt100@icloud.com>
2024-06-23 16:28:02 -0300

    Arrays/loops

---
+++ array/array.js
@@ -0,0 +1,9 @@
+var videoGamer = ['Switch', 'PS4', 'XBOX'];
+
+console.log(videoGamer[0]);
+
+// Métodos e Propriedades de um Array
+
+videoGamer.pop(); // Remove o último item e retorna ele
+videoGamer.push('3DS'); // Adiciona ao final do array
+videoGamer.length; // 3
---
+++ array/for loop, while loop/forwhile.js
@@ -0,0 +1,10 @@
+for (var numero = 0; numero < 10; numero++) {
+    console.log(numero);
+}
+
+
+var i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
